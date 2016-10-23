12a03c7dd7be8d64be0ded37cc10532d0ce41d80
Niklas Sjoquist <nhs4@duke.edu>
2016-10-23 16:53:08 -0400

    Updated Observers and moved drawing capability to TurtleObserver

--- src/frontend/observers/PositionObserver.java
+++ src/frontend/observers/PositionObserver.java
@@ -3,6 +3,9 @@ package frontend.observers;
 import java.util.Observable;
 import backend.observables.ObservablePosition;
 import frontend.center.TurtleMascot;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
 
 /**
  * @author Niklas Sjoquist
@@ -11,24 +14,25 @@ import frontend.center.TurtleMascot;
 public class PositionObserver extends TurtleObserver {
     private ObservablePosition turtlePosition = null;
 
-    public PositionObserver(TurtleMascot turtle, ObservablePosition position) {
-            super(turtle);
+    public PositionObserver(TurtleMascot turtle, GraphicsContext gcc, ObservablePosition position) {
+            super(turtle,gcc);
             turtlePosition = position;
     }
 
     public void update(Observable obs, Object obj) {
             if (obs == turtlePosition) {
-                    // TODO: Move Turtle to updated position
-                    //               Check if pen is down, maybe draw line
+                    // Move Turtle to updated position;
+                    // Check if pen is down, maybe draw line
                     ObservablePosition pos = (ObservablePosition) obs;
                     setPosition(pos.getPosition());
                     
-                    if (/*pen is down*/) {
+                    if (getPenStatus()) {
                             // TODO: draw line to new position
-                            
+                            addPathElement(new LineTo(pos.getX(),pos.getY()));
                     }
                     else {
                             // TODO: move path to new position (moveTo) or store a previous position
+                            addPathElement(new MoveTo(pos.getX(),pos.getY()));
                     }
             }
     }
